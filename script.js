09881f42ed3169e3158f4289763e6a71cb4a5254
rahulsing23 <rahul561singh@gmail.com>
2024-01-07 20:52:40 +0530

    [ADD] File Added

---
+++ script.js
@@ -0,0 +1,23 @@
+const jokes = document.querySelector('.joke');
+const btn = document.getElementById('btn');
+
+const generateJokes = async () => {
+    try {
+        const setHeader = {
+            headers: {
+                Accept: "application/json"
+            }
+        }
+        // Fetching Api with async await
+        const url = "https://icanhazdadjoke.com";
+        const res = await fetch(url, setHeader);
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+    }
+    catch (error) {
+        console.log(`The error is ${error}`);
+    }
+}
+
+// Calling a function
+btn.addEventListener('click', generateJokes);
